c8b2c745d2d74d84da098df369754a4f4b6e3c12
Karl Gusner <karlgusner@gmail.com>
2012-05-06 16:06:13 -0400

    heroku error fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ end
 
 module Farrah
   class Application < Rails::Application
+    
+    # to fix could not connect to server: Connection refused heroku errors
+    config.assets.initialize_on_precompile = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
